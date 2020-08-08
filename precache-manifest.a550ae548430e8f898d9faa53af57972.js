9f63227cb644e2409a9b6f2b03df4acaba1762c4
asharma1398 <asharma1398@gmail.com>
2020-08-07 21:36:24 -0400

    Updates

---
+++ precache-manifest.a550ae548430e8f898d9faa53af57972.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f3d07d9d162ba2307fd7df747d44c4",
+    "url": "/Money-Heist-Employee-Directory/index.html"
+  },
+  {
+    "revision": "101b577a40a3ccc2638b",
+    "url": "/Money-Heist-Employee-Directory/static/css/main.b993d862.chunk.css"
+  },
+  {
+    "revision": "6197d4c0de3f8e4c45cc",
+    "url": "/Money-Heist-Employee-Directory/static/js/2.8dea0d83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Money-Heist-Employee-Directory/static/js/2.8dea0d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101b577a40a3ccc2638b",
+    "url": "/Money-Heist-Employee-Directory/static/js/main.0fd9bb8f.chunk.js"
+  },
+  {
+    "revision": "4d403d10a81362b482ec",
+    "url": "/Money-Heist-Employee-Directory/static/js/runtime-main.a116d696.js"
+  },
+  {
+    "revision": "c2ecc3870561323b0a758c43d3cfb810",
+    "url": "/Money-Heist-Employee-Directory/static/media/moneyHeist.c2ecc387.png"
+  }
+]);
